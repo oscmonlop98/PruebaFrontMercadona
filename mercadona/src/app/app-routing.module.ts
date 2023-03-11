4b3c0fe7425dea413fed350953afcbd889f9cdeb
oscmonlop98 <oscarmorenolopinfo2@gmail.com>
2023-03-11 02:39:59 +0100

    Deleted unused component

--- mercadona/src/app/app-routing.module.ts
+++ mercadona/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ConfigTornillosComponent } from './modules/config-tornillos/config-tornillos.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) },
-  { path: 'config-tornillos', component: ConfigTornillosComponent}
+  { path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) }
 ];
 
 @NgModule({
